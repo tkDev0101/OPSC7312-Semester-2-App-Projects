ae7e17c34563176b43a77435cb8beaeddfa27af5
tkDev <153438920+tkDev0101@users.noreply.github.com>
2024-08-22 06:12:26 +0200

    OPSC7312 POE Part 1 Demo
    Added project OPSC7312 POE Part 1 Demo

---
+++ OPSC7312POEPart1Demo/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "OPSC7312 POE Part 1 - Demo"
+include(":app")
+ 
